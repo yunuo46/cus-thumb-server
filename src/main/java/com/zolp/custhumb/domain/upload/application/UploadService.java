c9911b22f695549934581e252c274256372d1c24
Hyunwoo Song <yunuo468@gmail.com>
2025-06-18 15:39:41 +0900

    fix: apply KST for timestamp

--- src/main/java/com/zolp/custhumb/domain/upload/application/UploadService.java
+++ src/main/java/com/zolp/custhumb/domain/upload/application/UploadService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Service
@@ -18,27 +20,27 @@ public class UploadService {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
 
     public UploadResponse generateVideoAndTextUrls(Long userId, String fileName, String bucket) {
-        String timestamp = LocalDateTime.now().format(FORMATTER);
+        String baseTimestamp = ZonedDateTime.now(ZoneId.of("Asia/Seoul")).format(FORMATTER);
         String baseName = getBaseName(fileName);
 
         String videoObject = "video/" + baseName + ".mp4";
         String textObject = "video/" + baseName + ".txt";
 
-        URL videoUrl = gcsSignedUrlService.generateUploadUrl(bucket, videoObject, "video/mp4", 15, userId, timestamp, 0);
-        URL textUrl = gcsSignedUrlService.generateUploadUrl(bucket, textObject, "text/plain", 5, userId, timestamp, 0);
+        URL videoUrl = gcsSignedUrlService.generateUploadUrl(bucket, videoObject, "video/mp4", 15, userId, baseTimestamp, 0);
+        URL textUrl = gcsSignedUrlService.generateUploadUrl(bucket, textObject, "text/plain", 5, userId, baseTimestamp, 0);
 
         return new UploadResponse(videoUrl.toString(), textUrl.toString());
     }
 
     public UploadResponse generateImageAndTextUrls(Long userId, String fileName, String bucket) {
-        String timestamp = LocalDateTime.now().format(FORMATTER);
+        String baseTimestamp = ZonedDateTime.now(ZoneId.of("Asia/Seoul")).format(FORMATTER);
         String baseName = getBaseName(fileName);
 
         String imageObject = "image/" + baseName + ".png";
         String textObject = "image/" + baseName + ".txt";
 
-        URL imageUrl = gcsSignedUrlService.generateUploadUrl(bucket, imageObject, "image/png", 5, userId, timestamp, 0);
-        URL textUrl = gcsSignedUrlService.generateUploadUrl(bucket, textObject, "text/plain", 5, userId, timestamp, 0);
+        URL imageUrl = gcsSignedUrlService.generateUploadUrl(bucket, imageObject, "image/png", 5, userId, baseTimestamp, 0);
+        URL textUrl = gcsSignedUrlService.generateUploadUrl(bucket, textObject, "text/plain", 5, userId, baseTimestamp, 0);
 
         return new UploadResponse(imageUrl.toString(), textUrl.toString());
     }
